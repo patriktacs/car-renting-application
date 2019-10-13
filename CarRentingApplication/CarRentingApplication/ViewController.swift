2b606cecaecd3288d24e788ebdf422ba31b5a694
patriktacs <acs.patrik@simonyi.bme.hu>
2019-10-13 15:30:08 +0200

    Project initiation

---
+++ CarRentingApplication/CarRentingApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarRentingApplication
+//
+//  Created by Ács Patrik Tamás on 2019. 10. 13..
+//  Copyright © 2019. Ács Patrik Tamás. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
