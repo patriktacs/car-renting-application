c80b041226c6aed9db0eb4a3815959c9b72fdef6
Ács Patrik <patrik.acs1@vodafone.com>
2019-10-20 17:12:46 +0200

    TEMA-15 feature - create PersistencyManager

---
+++ CarRentingApplication/Assembly/PersistencyAssembly.swift
@@ -0,0 +1,17 @@
+//
+//  PersistencyAssembly.swift
+//  CarRentingApplication
+//
+//  Created by Ács Patrik on 2019. 10. 20..
+//  Copyright © 2019. Ács Patrik Tamás. All rights reserved.
+//
+
+import Foundation
+import Swinject
+import SwinjectAutoregistration
+
+class PersistencyAssembly: Assembly {
+    func assemble(container: Container) {
+        container.autoregister(PersistingManager.self, initializer: PersistencyManager.init)
+    }
+}
