f32b80c85a29c6dfb2889b3fa28f39a41fb07d5b
Ács, Patrik, Vodafone Hungary <patrik.acs3@vodafone.com>
2019-11-07 00:04:03 +0100

    TEMA-30 bugfix - add input validation on first reg screen, rework first and second reg ui

--- CarRentingApplication/UseCase/Registration/AccountDataInput/AccountDataInputViewController.swift
+++ CarRentingApplication/UseCase/Registration/AccountDataInput/AccountDataInputViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 import RxBiBinding
+import RxSwift
 
 class AccountDataInputViewController: UIViewController {
     
+    
+    @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var emailTextField: TextField!
+    @IBOutlet weak var passwordLabel: UILabel!
     @IBOutlet weak var passwordTextField: TextField!
+    @IBOutlet weak var repeatPasswordLabel: UILabel!
     @IBOutlet weak var repeatPasswordtextField: TextField!
     @IBOutlet weak var nextButton: Button!
     
@@ -22,11 +27,22 @@ class AccountDataInputViewController: UIViewController {
         super.viewDidLoad()
         
         setupCancel()
+        setupInputValidation()
         hideKeyboardWhenTappedAround()
         
+        emailLabel.text = " "
+        emailLabel.textColor = .red
+        passwordLabel.text = " "
+        passwordLabel.textColor = .red
+        repeatPasswordLabel.text = " "
+        repeatPasswordLabel.textColor = .red
+        
         emailTextField.setupData(placeholder: "Email")
+        emailTextField.delegate = self
         passwordTextField.setupData(placeholder: "Password", secureTextEntry: true)
+        passwordTextField.delegate = self
         repeatPasswordtextField.setupData(placeholder: "Repeat password", secureTextEntry: true)
+        repeatPasswordtextField.delegate = self
 
         nextButton.setupData(title: "Next")
         
@@ -44,9 +60,11 @@ class AccountDataInputViewController: UIViewController {
     }
     
     @IBAction func nextButtonAction(_ sender: Any) {
-        let loginStoryboard = UIStoryboard(name: "Login", bundle: nil)
-        let personalDataInputViewController = loginStoryboard.instantiateViewController(withIdentifier: "PersonalDataInput")
-        self.navigationController?.pushViewController(personalDataInputViewController, animated: true)
+        if inputValidation() {
+            let loginStoryboard = UIStoryboard(name: "Login", bundle: nil)
+            let personalDataInputViewController = loginStoryboard.instantiateViewController(withIdentifier: "PersonalDataInput")
+            self.navigationController?.pushViewController(personalDataInputViewController, animated: true)
+        }
     }
     
     func setupCancel() {
@@ -57,5 +75,73 @@ class AccountDataInputViewController: UIViewController {
     @objc func cancelRegistration(sender: UIBarButtonItem) {
         viewModel.cancelRegistration()
         self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    func inputValidation() -> Bool {
+        let emailRegexString = "[A-Z0-9a-z._-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailRegex = NSPredicate(format:"SELF MATCHES %@", emailRegexString)
+        let emailValidation = emailRegex.evaluate(with: emailTextField.text)
+        
+        let passwordRegexString = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[A-Z0-9a-z]{6,}"
+        let passwordRegex = NSPredicate(format:"SELF MATCHES %@", passwordRegexString)
+        let passwordValidation = passwordRegex.evaluate(with: passwordTextField.text)
+        
+        let repeatPasswordValidation = self.repeatPasswordtextField.text == self.passwordTextField.text
+        
+        if !emailValidation {
+            emailLabel.text = "Email format is 'someone@somewhere.com'"
+        } else {
+            emailLabel.text = " "
+        }
+        
+        if !passwordValidation {
+            passwordLabel.text = "At least 6 characters one upper, one lowercase letter and a number."
+        } else {
+            passwordLabel.text = " "
+        }
+        
+        if !repeatPasswordValidation {
+            repeatPasswordLabel.text = "Passwords doesn't match."
+        } else {
+            repeatPasswordLabel.text = " "
+        }
+        
+        return emailValidation && passwordValidation && repeatPasswordValidation
+    }
+    
+    func setupInputValidation() {
+        let emailRegexString = "[A-Z0-9a-z._-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailRegex = NSPredicate(format:"SELF MATCHES %@", emailRegexString)
+        
+        let passwordRegexString = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[A-Z0-9a-z]{6,}"
+        let passwordRegex = NSPredicate(format:"SELF MATCHES %@", passwordRegexString)
+        
+        let emailEditEnd = emailTextField.rx.controlEvent(.editingDidEnd).asObservable().startWith(())
+        let passwordEditEnd = passwordTextField.rx.controlEvent(.editingDidEnd).asObservable().startWith(())
+        let repeatPasswordEditEnd = repeatPasswordtextField.rx.controlEvent(.editingDidEnd).asObservable().startWith(())
+        
+        Observable.combineLatest(emailEditEnd, passwordEditEnd, repeatPasswordEditEnd)
+            .map { _, _, _ in
+                let emailValidation = emailRegex.evaluate(with: self.emailTextField.text) || self.emailTextField.text == ""
+                let passwordValidation = passwordRegex.evaluate(with: self.passwordTextField.text) || self.passwordTextField.text == ""
+                let repeatPasswordValidation = self.repeatPasswordtextField.text == self.passwordTextField.text || self.repeatPasswordtextField.text == ""
+        
+                if !emailValidation {
+                    self.emailLabel.text = "Email format is 'someone@somewhere.com'"
+                } else {
+                    self.emailLabel.text = " "
+                }
+        
+                if !passwordValidation {
+                    self.passwordLabel.text = "At least 6 characters one upper, one lowercase letter and a number."
+                } else {
+                    self.passwordLabel.text = " "
+                }
+        
+                if !repeatPasswordValidation {
+                    self.repeatPasswordLabel.text = "Passwords doesn't match."
+                } else {
+                    self.repeatPasswordLabel.text = " "
+        }}.subscribe().disposed(by: rx.disposeBag)
     }
 }
